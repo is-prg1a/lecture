c043d9e0cba94bccfbcb74190188b4a1559a9739
Ken Wakita <wakita@is.titech.ac.jp>
2016-09-21 22:56:25 +0900

    CSVっぽく出力するように変更しました．
    古い版はJSONの扱いの記録のためにregisterJSON.scalaとして残しました．
    
    Changes to be committed:
    new file:   src/lx00/register.scala
    renamed:    src/lx00/registration.scala -> src/lx00/registerJSON.scala

--- src/lx00/register.scala
+++ src/lx00/register.scala
@@ -1,7 +1,3 @@
-package lx00
-
-import net.liftweb.json._
-
 /**
  * このプログラムの val information = ... で始まる登録情報の文字列修正して
  * あなたの情報に書き換えてから、このプログラムを実行して下さい。
@@ -9,7 +5,8 @@ import net.liftweb.json._
  * 修正したプログラムを ???@is.titech.ac.jp への添付メールとして提出しなさい。
  **/
 
-object Registration {
+object Register {
+  /*
   val 登録情報 = """{
     "family": "あなたの姓に書き換えて下さい",
     "name":   "あなたの名に書き換えて下さい",
@@ -17,10 +14,21 @@ object Registration {
     "github": "あなたのGitHubアカウント名に書き換えて下さい",
     "login":  "あなたのログインIDに書き換えて下さい",
     "email":  "あなたの東工大メールのアドレスの@から左の部分に書き換えて下さい" }"""
+  */
+  val 登録情報 = Map(
+    "family" -> "あなたの姓に書き換えて下さい",
+    "name"   -> "あなたの名に書き換えて下さい",
+    "id"     -> "あなたの学籍番号に書き換えて下さい",
+    "github" -> "あなたのGitHubアカウント名に書き換えて下さい",
+    "login"  -> "あなたのログインIDに書き換えて下さい",
+    "email"  -> "あなたの東工大メールのアドレスの@から左の部分に書き換えて下さい")
 
   def main(arguments: Array[String]) {
+    println(登録情報.values.mkString(","))
+    /*
     println("\n\n----------------------------------------------------------------------")
     println(prettyRender(parse(登録情報)))
     println("----------------------------------------------------------------------\n\n")
+    */
   }
 }
